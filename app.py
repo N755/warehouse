2eaaf1ea595670aa2b4338cf17d9b2841deaf497
Anastasiia Sirenko <nastya9701@ukr.net>
2023-04-24 20:34:57 +0300

    warehouse

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, request,redirect, url_for
+from flask import Flask, render_template, request,redirect, url_for, make_response 
 from typing import List, Dict
-from forms import ProductForm
+from forms import ProductForm, ProductSaleForm
+import csv
+import io
+
 
 app = Flask(__name__)
 app.secret_key  = 'secret'
@@ -29,7 +32,10 @@ ITEMS = [
 
 @app.route('/products', methods=['GET'])
 def product_list():
-    return render_template('product_list.html', products=ITEMS)
+    form = ProductForm(request.form)
+    for item in ITEMS:
+        item.form = ProductSaleForm()
+    return render_template('product_list.html', form=form, ITEMS=ITEMS)
 
 @app.route('/add', methods=['POST'])
 def add_product():
@@ -42,7 +48,50 @@ def add_product():
         product = Product(name, unit, unit_price, quantity)
         ITEMS.append(product)
         print('hi')
-        return render_template('add.html', form=form, ITEMS=ITEMS)
-    return 'Invalid form submission'
+        return redirect(url_for('product_list'))
+    else:
+        return render_template('product_list.html', form=form, ITEMS=ITEMS)
+
+@app.route('/sell/<product_name>', methods=['GET', 'POST'])
+def sell_product(product_name):
+    product = next((item for item in ITEMS if item.name == product_name), None)
+    if product is None:
+        return redirect(url_for('product_list'))
+    form = ProductSaleForm(request.form)
+    if request.method == 'POST' and form.validate():
+        sold_quantity = form.quantity.data
+        product.quantity -= sold_quantity
+        return redirect(url_for('product_list'))
+    return render_template('sell_product.html', product=product, form=form)
+
+
+
+@app.route('/import', methods=['GET', 'POST'])
+def import_data():
+    if request.method == 'POST':
+        file = request.files['file']
+        stream = io.StringIO(file.stream.read().decode("UTF8"), newline=None)
+        csv_input = csv.reader(stream)
+        next(csv_input)
+        for row in csv_input:
+            name, unit, unit_price, quantity = row
+            product = Product(name, unit, float(unit_price), float(quantity))
+            ITEMS.append(product)
+        return redirect(url_for('product_list'))
+    return render_template('import_data.html')
+
+@app.route('/export', methods=['GET'])
+def export_data():
+    data = [['Name', 'Unit', 'Unit Price', 'Quantity']]
+    for item in ITEMS:
+        data.append([item.name, item.unit, item.unit_price, item.quantity])
+    si = io.StringIO()
+    cw = csv.writer(si)
+    cw.writerows(data)
+    output = make_response(si.getvalue())
+    output.headers["Content-Disposition"] = "attachment; filename=export.csv"
+    output.headers["Content-type"] = "text/csv"
+    return output
+
 if __name__ == '__main__':
     app.run(debug=True)
--- forms.py
+++ forms.py
@@ -1,7 +1,11 @@
-from wtforms import Form, StringField, DecimalField, validators
+from wtforms import Form, StringField, DecimalField, SubmitField, validators
 
 class ProductForm(Form):
     name = StringField('Name', [validators.Length(min=1, max=50)])
     unit = StringField('Unit', [validators.Length(min=1, max=20)])
     unit_price = DecimalField('Unit_price', [validators.NumberRange(min=0.1, max=9999)])
     quantity = DecimalField('Quantity', [validators.NumberRange(min=0.1, max=9999)])
+
+class ProductSaleForm(Form):
+    quantity = DecimalField('Quantity', [validators.NumberRange(min=0.1, max=9999)])
+    submit = SubmitField('Sell')
